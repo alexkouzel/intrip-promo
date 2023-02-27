6d9e3f71a70d90cb6c3f6626e5b089a917c74f63
Alexey Kovzel <alexey.kovzel@gmail.com>
2023-02-28 00:19:28 +0100

    Add burger menu

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -1,14 +1,26 @@
-// adapt for smaller laptop screens
-if (screen.width < 1700) {
-    document.querySelector('html').style.fontSize = '50%' 
-}
+document.addEventListener('DOMContentLoaded', function () {
 
-let scrollEvent;
-document.onscroll = (event) => {
-    clearTimeout(scrollEvent);
-    scrollEvent = setTimeout(() => {
-        let shadow = '0 0 10px 0 var(--purple)';
-        let header = document.querySelector('header');
-        header.style.boxShadow = window.scrollY > 0 ? shadow : 'none';
-    }, 50);
-}
+    // adapt for smaller laptop screens
+    if (screen.width < 1700) {
+        document.querySelector('html').style.fontSize = '50%'
+    }
+
+    // handle header shadowing on scroll
+    let scrollEvent;
+    document.onscroll = (event) => {
+        clearTimeout(scrollEvent);
+        scrollEvent = setTimeout(() => {
+            let nav = document.querySelector('.nav');
+            let shadow = '0 10px 10px -10px var(--purple)'
+            nav.style.boxShadow = window.scrollY > 0 ? shadow : 'none';
+        }, 50);
+    }
+
+    // handle burger menu for mobile
+    this.querySelector('.burger').onclick = function () {
+        let burger = document.querySelector('.burger-input');
+        let nav = document.querySelector('.nav');
+        nav.style.maxHeight = burger.checked ? '9rem' : '45rem';
+        burger.checked = !burger.checked;
+    };
+});
